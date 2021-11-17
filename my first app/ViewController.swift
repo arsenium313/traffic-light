5c694a6078487dfd41c2a52fac3c9799c0c424ef
arsenium313 <arsenium313@gmail.com>
2021-11-17 16:27:48 +0300

    Initial Commit

---
+++ my first app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  my first app
+//
+//  Created by Арсений Кухарев on 17.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
