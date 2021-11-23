22d9992b51016d1c2791a4dd4a8c932d4b411836
arsenium313 <arsenium313@gmail.com>
2021-11-23 17:54:39 +0300

    write logic

--- my first app/ViewController.swift
+++ my first app/ViewController.swift
@@ -1,40 +1,56 @@
-//
-//  ViewController.swift
-//  my first app
-//
-//  Created by Арсений Кухарев on 17.11.2021.
-//
-
 import UIKit
+
+enum lightColors{
+    case red, yellow, green
+}
 
 class ViewController: UIViewController {
 
     @IBOutlet var redLight: UIView!
-    @IBOutlet weak var greenLight: UIView!
-    @IBOutlet weak var yellowLight: UIView!
+    @IBOutlet var greenLight: UIView!
+    @IBOutlet var yellowLight: UIView!
+    @IBOutlet var buttonOutlet: UIButton!
     
-    @IBOutlet weak var buttonOutlet: UIButton!
+    let lightOn: CGFloat = 1
+    let lightOff: CGFloat = 0.1
     
- 
+    var currentLight = lightColors.red
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        redLight.alpha = 0.5
         buttonOutlet.layer.cornerRadius = 10
-    }
-    
-    @IBAction func pressAction(_ sender: Any) {
-        
+        redLight.alpha = lightOff
+        yellowLight.alpha = lightOff
+        greenLight.alpha = lightOff
     }
     
     override func viewWillLayoutSubviews() {
         redLight.layer.cornerRadius = redLight.frame.width / 2
-        redLight.clipsToBounds = true
+        yellowLight.layer.cornerRadius = redLight.frame.width / 2
+        greenLight.layer.cornerRadius = redLight.frame.width / 2
     }
     
+    @IBAction func nextColor() {
+        buttonOutlet.setTitle("Next", for: UIControl.State.normal)
+        switch currentLight {
+        case .red:
+            redLight.alpha = lightOn
+            currentLight = .yellow
+            yellowLight.alpha = lightOff
+            greenLight.alpha = lightOff
+        case .yellow:
+            yellowLight.alpha = lightOn
+            currentLight = .green
+            redLight.alpha = lightOff
+            greenLight.alpha = lightOff
+        case .green:
+            greenLight.alpha = lightOn
+            currentLight = .red
+            redLight.alpha = lightOff
+            yellowLight.alpha = lightOff
+        }
+    }
     
-
     
 }
 
