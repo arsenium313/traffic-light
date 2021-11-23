74dd9f4bf0fcb33177273155e7f52bb4377512ae
arsenium313 <arsenium313@gmail.com>
2021-11-23 16:31:55 +0300

    create a view

--- my first app/ViewController.swift
+++ my first app/ViewController.swift
@@ -9,11 +9,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var redLight: UIView!
+    @IBOutlet weak var greenLight: UIView!
+    @IBOutlet weak var yellowLight: UIView!
+    
+    @IBOutlet weak var buttonOutlet: UIButton!
+    
+ 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        redLight.alpha = 0.5
+        buttonOutlet.layer.cornerRadius = 10
     }
+    
+    @IBAction func pressAction(_ sender: Any) {
+        
+    }
+    
+    override func viewWillLayoutSubviews() {
+        redLight.layer.cornerRadius = redLight.frame.width / 2
+        redLight.clipsToBounds = true
+    }
+    
+    
 
-
+    
 }
 
